996d797bff0c44eaf015ac0f97f5d2d970533a1f
Milivoje Pualic <milivojepualic@Milivojes-MacBook-Air.local>
2022-01-27 15:02:50 +0100

    create env file to store veriables

--- database/connect.js
+++ database/connect.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+
 
 function getMySQLConnection() {
     return mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "Programer25@",
-        database: "code_academy"
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE
     });
 }
 
